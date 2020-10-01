e2df982e58773148d8827c264de46393db74e7e7
hborkar98 <70790226+hborkar98@users.noreply.github.com>
2020-10-01 11:25:04 +0530

    Create stackDemo.cs

---
+++ stackDemo.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+
+namespace fundamentals
+{
+    class stackDemo
+    {
+        private static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            string s1 = "hiii";
+            string s2 = "hello";
+            st.Push(5);
+            st.Push(6);
+            st.Push(s1);  //push in stack
+            st.Push(s2);
+
+            foreach (var ie in st)
+            {                                             //use var in for loop with stack object
+                Console.WriteLine(ie);                    //retrieving from stack 
+
+            }
+
+            if (st.Contains(6))                           // contains function in stack
+                Console.WriteLine("Contains");
+
+            Console.WriteLine(st.Peek());                //peek function in stack
+
+            Console.WriteLine(st.Pop().ToString());      //pop function in stack 
+        }
+    }
+
+}
